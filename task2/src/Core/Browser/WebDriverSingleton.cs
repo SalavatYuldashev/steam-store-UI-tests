9c3effcc126792e104dc345204805d32a5340673
SalavatYuldashev <salavat.yuldashev@vk.com>
2025-08-20 10:02:02 +0300

    added config reader and singleton

--- task2/src/Core/Browser/WebDriverSingleton.cs
+++ task2/src/Core/Browser/WebDriverSingleton.cs
@@ -1,6 +1,42 @@
-﻿namespace DefaultNamespace;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using task2.Core.Config;
 
-public class WebDriverSingleton
+namespace task2.Core.Browser;
+
+public sealed class WebDriverSingleton
 {
-    
+    private static IWebDriver? _driver;
+    private static TestSettings? _settings;
+
+    public static IWebDriver Driver
+    {
+        get
+        {
+            if (_driver == null)
+            {
+                var options = new ChromeOptions();
+                if (_settings.Headless)
+                {
+                    options.AddArgument("--headless");
+                }
+
+                _driver = new ChromeDriver(options);
+            }
+
+            return _driver;
+        }
+    }
+
+    public static void Configure(TestSettings testSettings)
+    {
+        _settings = testSettings;
+    }
+
+    public static void Quit()
+    {
+        _driver?.Quit();
+        _driver?.Dispose();
+        _driver = null;
+    }
 }
---
+++ task2/src/Core/Config/ConfigReader.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace task2.Core.Config;
+
+public class ConfigReader
+{
+    public static TestSettings load()
+    {
+        var configurationRoot = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+        var testSettings = new TestSettings();
+        configurationRoot.Bind(testSettings);
+
+        return testSettings;
+    }
+}
